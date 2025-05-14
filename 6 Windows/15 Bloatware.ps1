7449da780be4c4832fa1a69b82c2e6c771821ce0
FR33THY <fr33thy@gmail.com>
2025-05-14 13:36:49 +1000

    Update 15 Bloatware.ps1

--- 6 Windows/15 Bloatware.ps1
+++ 6 Windows/15 Bloatware.ps1
@@ -414,7 +414,7 @@ show-menu
 Clear-Host
 Write-Host "Installing: Remote Desktop Connection. Please wait . . ."
 # download remote desktop connection
-Get-FileFromWeb -URL "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RW1ePwb" -File "$env:TEMP\RemoteDesktopConnection.exe"
+Get-FileFromWeb -URL "https://go.microsoft.com/fwlink/?linkid=2247659" -File "$env:TEMP\RemoteDesktopConnection.exe"
 # install remote desktop connection 
 cmd /c "%TEMP%\RemoteDesktopConnection.exe >nul 2>&1"
 Clear-Host
